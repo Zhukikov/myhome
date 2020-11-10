8ad43df20378f3637d7185fc7cd8f269446fb421
Andrejs Muzikovs <andrejs.muzikovs@gmail.com>
2020-11-10 20:46:17 +0100

    Cleanup

--- modules/sway/sway.nix
+++ modules/sway/sway.nix
@@ -3,17 +3,7 @@
 {
   environment = {
     etc = {
-      #"sway/config" = {
-      #  text = ''
-      #    bar {
-      #      swaybar_command waybar
-      #    } 
-      #  '';
-      #};
-      #"xdg/waybar/config".source = ./dotfiles/waybar/config;
-      #"xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
       "sway/config".source = ./sway_config;
-      "xdg/waybar/config".source = ./waybar_config;
     };
   };
 }
