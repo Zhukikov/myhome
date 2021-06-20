22a6f5be3260439114996b708a3bbf9fdbb85337
Andrejs Muzikovs <andrejs.muzikovs@gmail.com>
2021-06-20 13:42:25 +0200

    Configure t14s machine

--- machines/common.nix
+++ machines/common.nix
@@ -18,7 +18,6 @@
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.enp0s25.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
 
   # Configure network proxy if necessary
@@ -38,7 +37,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget nano firefox-wayland tree ag lsof git idea.idea-community maven jdk11 gnumake tig gparted acpi htop freecad hibernate arduino php74Packages.composer redis pstree lm_sensors cpufrequtils font-awesome waybar pavucontrol wirelesstools iw networkmanager neofetch fzf brightnessctl jq wev wdisplays wofi hicolor-icon-theme
+    wget nano firefox-wayland tree ag lsof git idea.idea-community maven jdk11 gnumake tig gparted acpi htop freecad hibernate arduino php74Packages.composer redis pstree lm_sensors cpufrequtils font-awesome waybar pavucontrol wirelesstools iw networkmanager neofetch fzf brightnessctl jq wev wdisplays wofi hicolor-icon-theme _1password _1password-gui
     (import ../modules/vim.nix)
   ];
 
@@ -102,14 +101,8 @@
     };
   }];
 
+  nixpkgs.config.allowUnfree = true;
+
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
 }
--- machines/t14s/configuration.nix
+++ machines/t14s/configuration.nix
@@ -7,20 +7,18 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      <nixos-hardware/lenovo/thinkpad/t450s>
+      <nixos-hardware/lenovo/thinkpad/t14s/amd/gen1>
       ./hardware-configuration.nix
-      (import ../common.nix {config = config; pkgs = pkgs; username = "andrejs"; hostname = "t450s";})
+      (import ../common.nix {config = config; pkgs = pkgs; username = "andrejs"; hostname = "t14s";})
     ];
 
+  # luks
   boot.initrd.luks.devices = {
-    root = {
-      device = "/dev/disk/by-uuid/9e8daa89-dcb9-4c87-bdae-8f8813b665c6";
+    crypted = {
+      device = "/dev/disk/by-uuid/c615ec42-d4f5-4378-8e61-edbb1a284cf4";
       preLVM = true;
-      allowDiscards = true;
     };
   };
-
-  networking.interfaces.enp0s25.useDHCP = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -28,5 +26,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "21.05"; # Did you read the comment?
 }
