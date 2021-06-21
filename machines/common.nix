3ae140d86e3d7d8cf23186b81d80c78dd85c820c
Andrejs Muzikovs <andrejs.muzikovs@gmail.com>
2021-06-21 11:52:33 +0200

    Add Slack

--- machines/common.nix
+++ machines/common.nix
@@ -37,7 +37,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget nano firefox-wayland tree ag lsof git idea.idea-community maven jdk11 gnumake tig gparted acpi htop freecad hibernate arduino php74Packages.composer redis pstree lm_sensors cpufrequtils font-awesome waybar pavucontrol wirelesstools iw networkmanager neofetch fzf brightnessctl jq wev wdisplays wofi hicolor-icon-theme _1password _1password-gui
+    wget nano firefox-wayland tree ag lsof git idea.idea-community maven jdk11 gnumake tig gparted acpi htop freecad hibernate arduino php74Packages.composer redis pstree lm_sensors cpufrequtils font-awesome waybar pavucontrol wirelesstools iw networkmanager neofetch fzf brightnessctl jq wev wdisplays wofi hicolor-icon-theme _1password _1password-gui slack xwayland
     (import ../modules/vim.nix)
   ];
 
