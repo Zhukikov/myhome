c765d0be24273a2e9ddee9f41476fb05a029a6b5
Andrejs Muzikovs <andrejs.muzikovs@gmail.com>
2022-12-16 18:09:56 +0100

    Update t14s hardware config

--- machines/t14s/hardware-configuration.nix
+++ machines/t14s/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
